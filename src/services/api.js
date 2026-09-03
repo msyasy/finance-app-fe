49d17c597efcd4a4c6a23716524dc9e3e26f16c5
Muhammad Syaifulloh <msyasy.care@gmail.com>
2026-09-03 13:29:09 +0700

    Fix API baseURL using VITE_API_URL env

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
 });
 
 // Otomatis menempelkan Token JWT di setiap request jika user sudah login
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
