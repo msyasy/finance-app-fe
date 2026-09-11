03dc75bc926e36cd74615790a94c5b01ebfff32e
Muhammad Syaifulloh <msyasy.care@gmail.com>
2026-09-11 23:44:56 +0700

    feat(pwa): finalize pwa configuration and fix icon paths

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'icons.svg'],
+      devOptions: {
+        enabled: true // Mengaktifkan PWA di lokal
+      },
       manifest: {
         name: 'Lapkeu - Laporan Keuangan',
         short_name: 'LapKeu',
@@ -21,17 +24,17 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
