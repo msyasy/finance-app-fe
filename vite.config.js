0cc72ce107caca91ca85194fd71890c6b1151766
Muhammad Syaifulloh <msyasy.care@gmail.com>
2026-09-11 23:29:31 +0700

    feat(pwa): update vite config and index html for pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'icons.svg'],
+      manifest: {
+        name: 'Lapkeu - Laporan Keuangan',
+        short_name: 'LapKeu',
+        description: 'Aplikasi Pengelola Keuangan',
+        theme_color: '#0f172a',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
